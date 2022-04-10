e0f1d786442a3794d82aa0163587d056b918fc37
Daniel Montet <daniel@kune.africa>
2022-04-10 13:04:01 +0300

    bootstrapped urql  then added a query for books

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createClient, Provider, useQuery } from "urql";
+import { BrowserRouter } from "react-router-dom";
+
+const client = createClient({
+  url: "https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql",
+});
+
+const BookQuery = `
+  query {
+    book {
+      author
+      pages {
+        pageIndex
+        content
+        tokens {
+          position
+          value
+        }
+      }
+      title
+    }
+  }
+`;
+
+const Book = () => {
+  const [result, reexecuteQuery] = useQuery({ query: BookQuery });
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading....</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  return (
+    <ul>
+      {data.book.pages.map((page: any) => (
+        <li key={page.pageIndex}>{page.content}</li>
+      ))}
+    </ul>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={client}>
+      <Book />
+    </Provider>
   );
 }
 
