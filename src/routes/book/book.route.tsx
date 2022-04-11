b62a16ee1d6ff543ff48f7493ad4a68d5c455d4a
Daniel Montet <daniel@kune.africa>
2022-04-11 17:01:48 +0300

    added pagination

--- src/routes/book/book.route.tsx
+++ src/routes/book/book.route.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { GlobalContext, token } from "../../types/types";
 
@@ -7,8 +7,34 @@ export const Token: React.FunctionComponent = () => {
   return <h3>{params.slug}</h3>;
 };
 
+const Pagination = ({ totalContents, contentItemsPerPage, paginate }: any) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalContents / contentItemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      {pageNumbers.map((number) => (
+        <div key={number} className="page-item">
+          <span onClick={() => paginate(number)} className="page-link">
+            {number}
+          </span>
+        </div>
+      ))}
+    </nav>
+  );
+};
+
 const Book: React.FunctionComponent = () => {
   const { data, handleSplit } = useContext(GlobalContext);
+  const [page, setPage] = useState(0);
+  const pageLength = 2;
+  const pagesVisited = page * pageLength;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [contentItemsPerPage] = useState(2);
 
   const generateTokenLink = (content: string, tokens: token[]) => {
     if (content.length && tokens.length) {
@@ -25,21 +51,44 @@ const Book: React.FunctionComponent = () => {
         let { value } = tokens[index];
         return (
           <Link to={`/${value}`} key={index}>
-            <h3>{item}</h3>
+            <span>{item}</span>
           </Link>
         );
       });
 
       return links;
     }
-    return <span>I am empty</span>;
+    return <span></span>;
   };
+  // get current contents
+  const indexOfLastContentItems = currentPage * contentItemsPerPage;
+  const indexOfFirstContentItems =
+    indexOfLastContentItems - contentItemsPerPage;
+  const currentContent = data?.book?.pages
+    ?.slice(indexOfFirstContentItems, indexOfLastContentItems)
+    .map(({ content, pageIndex, tokens }: any) => {
+      return <p key={pageIndex}>{generateTokenLink(content, tokens)}</p>;
+    });
+
+  // console.log(currentContent);
+
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const displayBook = data?.book
+    ?.pages!.slice(pagesVisited, pagesVisited + pageLength)
+    .map(({ content, pageIndex, tokens }: any) => {
+      return <p key={pageIndex}>{generateTokenLink(content, tokens)}</p>;
+    });
 
   return (
     <section>
-      {data!.book!.pages!.map(({ content, pageIndex, tokens }: any) => {
-        return <p key={pageIndex}>{generateTokenLink(content, tokens)}</p>;
-      })}
+      <section>{currentContent}</section>
+      <Pagination
+        totalContents={data?.book?.pages?.length}
+        contentItemsPerPage={contentItemsPerPage}
+        paginate={paginate}
+      />
     </section>
   );
 };
